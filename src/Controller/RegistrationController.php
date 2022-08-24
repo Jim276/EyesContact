6d801c299a8565583e0788f1e9e9f0693cdf9f5f
Hady <sane_hady@hotmail.fr>
2022-08-24 16:32:44 +0200

    update profile avatar

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -60,7 +60,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
-            $this->addFlash('success', 'Votre message a été envoyé');
+            $this->addFlash('success', 'Inscription réussie');
             return $this->redirectToRoute('app_login');
         }
 
