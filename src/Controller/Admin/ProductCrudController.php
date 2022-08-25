4bd3c3d5684a032270121813f376c28adf73ba32
Hady <sane_hady@hotmail.fr>
2022-08-25 17:02:12 +0200

    crud product image_product

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use DateTime;
 use App\Entity\Product;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -22,13 +25,20 @@ class ProductCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            TextField::new('name'),
             AssociationField::new('category'),
             AssociationField::new('supplier'),
-            TextField::new('name'),
             MoneyField::new('price', 'Price')->setCurrency('EUR'),
             TextareaField::new('description'),
+            ImageField::new('image_product')
+            ->setBasePath('products/')
+                ->setUploadDir('public/uploads/products')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(true),
             NumberField::new('weight'),
             SlugField::new('slug')->setTargetFieldName('name'),
+            DateTimeField::new('creationDate'),
+            DateTimeField::new('updateDate'), 
         ];
     }
 
