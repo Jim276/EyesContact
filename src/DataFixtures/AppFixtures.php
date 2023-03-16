b062c92fcbe9014c0650b56c1f388caa01669b96
cs-hsane <hady.sane@baulders.com>
2023-03-16 22:52:32 +0100

    fixtures create admin

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager):void
+    {
+        $admin = new User();
+        $admin->setFirstname('Admin');
+        $admin->setLastname('Administrator');
+        $admin->setEmail('admin@example.com');
+        $admin->setPhoneNumber(0615151515);
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setIsBlocked(false);
+        $admin->setPassword($this->passwordHasher->hashPassword(
+            $admin,
+            'EyesContact331@'
+        ));
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
